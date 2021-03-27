892c248fb9bcda2949eb6e156d8b0858c000f855
KouSei089 <qq00891829@icloud.com>
2021-03-27 18:09:07 +0900

    admin-func

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
-  
+  before_action :require_admin
+
   def index
     @users = User.all
   end
@@ -26,7 +27,13 @@ class Admin::UsersController < ApplicationController
     
   end
 
+  private
+
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_path, unless current_user.admin?
   end
 end
