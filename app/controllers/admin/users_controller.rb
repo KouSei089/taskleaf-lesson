2e6b9eb253ea124f9814f45471cc4f0f19d7aaa5
KouSei089 <qq00891829@icloud.com>
2021-03-27 18:46:53 +0900

    admin-session-user完了

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,26 +5,32 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
-  def new
-    @user = User.new
+  def show
+    @user = User.find(params[:id])
   end
 
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
-    else
-      render :new
-    end
+  def new
+    @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def show
-    
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
@@ -34,6 +40,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def require_admin
-    redirect_to root_path, unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
   end
 end
