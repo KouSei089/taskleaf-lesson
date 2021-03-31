ad089939c16e1be7d1d7b81ce6ccc4513f630ba4
KouSei089 <qq00891829@icloud.com>
2021-03-31 22:30:38 +0900

    torbolinks

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbolinks:load', function(){
+document.addEventListener("turbolinks:load", function(){
   document.querySelectorAll('.delete').forEach(function(a){
     a.addEventListener('ajax:success', function(){
       var td = a.parentNode;
