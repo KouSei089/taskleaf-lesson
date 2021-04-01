de5ffa71007e57684b21942c27e7cdd03dc0cb4f
KouSei089 <qq00891829@icloud.com>
2021-04-01 20:34:23 +0900

    mouseover

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,3 +1,15 @@
+document.addEventListener('turbolinks:load', function(){
+  document.querySelectorAll('td').forEach(function(){
+    td.addEventListener('mouseover', function(e){
+      e.currentTarget.style.backgroundColor = '#eff';
+    });
+
+    td.addEventListener('mouseout', function(e){
+      e.currentTarget.style.backgroundColor = '';
+    });
+  });
+});
+
 document.addEventListener("turbolinks:load", function(){
   document.querySelectorAll('.delete').forEach(function(a){
     a.addEventListener('ajax:success', function(){
